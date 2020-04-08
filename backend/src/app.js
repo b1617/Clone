90b4502228ea84d5c9f71b4d4c1906ddb1d8d03b
Dhinesh <sattia.dhinesh@gmail.fr>
2020-04-09 00:50:02 +0200

    cut token routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-const HttpStatus = require('http-status-codes');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./config/mongodb');
 const userRouter = require('./routes/user.routes');
-const jwt = require('../src/jwt');
+const tokenRouter = require('./routes/token.routes');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -17,23 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
 
-app.post('/refreshToken', (req, res) => {
-  // need to check refresh token is valid*
-  const token = req.body.token;
-  const userId = req.body.userId;
-  jwt.isValidToken(token, process.env.JWT_REFRESH_KEY).then((result) => {
-    if (result) {
-      const accessToken = jwt.createAccessToken({ id: userId });
-      res.status(HttpStatus.OK).json({ accessToken });
-    }
-    res.status(HttpStatus.UNAUTHORIZED);
-  });
-});
-
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+app.use('/token', tokenRouter);
 app.use('/auth', userRouter);
 
 app.listen(PORT, () => {
---
+++ backend/src/routes/token.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const HttpStatus = require('http-status-codes');
+const jwt = require('../jwt');
+
+router.post('/', (req, res) => {
+  const { refreshToken, userId } = req.body;
+  jwt
+    .isValidToken(refreshToken, process.env.JWT_REFRESH_KEY)
+    .then((result) => {
+      if (result.sub === userId) {
+        const accessToken = jwt.createAccessToken({ id: userId });
+        res.status(HttpStatus.OK).json({ accessToken });
+      }
+      res.status(HttpStatus.NOT_FOUND);
+    })
+    .catch((err) => {
+      res.status(HttpStatus.UNAUTHORIZED).json(err);
+    });
+});
+
+module.exports = router;
